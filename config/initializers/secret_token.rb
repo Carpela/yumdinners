9d658731cacb58bb498ddc84b8e0995be6a553a6
Keiran <keiran@carpela.com>
2013-02-23 18:59:19 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yumdinners::Application.config.secret_token = '58e3dc16620a567a99da11eb568c359270331749edea0e6f2068aaa782f85eb55104ec75754d228b3e802cdb008cb33f1d1e847ca66a340b90b7befc2bf50d39'
